cb2ab8b2df0e8d5e3ef8ff9fa476eba3e69c0ce4
CUB3D <callumthom11@gmail.com>
2018-08-11 18:58:04 +0100

    More sound

--- src/WaveLogic.cpp
+++ src/WaveLogic.cpp
@@ -6,14 +6,11 @@
 #include "Scene/Scene.h"
 #include "Loader.h"
 #include "PathFollowerComponent.h"
-<<<<<<< HEAD
 #include "AntAiComponent.h"
-=======
 #include "HealthBar.h"
 #include "GameScene.h"
 
 
->>>>>>> 39fb205334cca551df73eae0e869add4c22a2665
 void WaveLogic::update(level level, Scene &scene) {
     if(spawnTimer.timerSpeed < 0) {
         spawnTimer.timerSpeed = level.waves[currentWave].delay;
@@ -23,21 +20,14 @@ void WaveLogic::update(level level, Scene &scene) {
     if(spawnTimer.isTickComplete()) { // Time to spawn new enemy
         printf("Spawning %d/%d/%d\n", currentWavePosition, level.waves[currentWave].count, spawnTimer.timerSpeed);
 
-<<<<<<< HEAD
-        currentWavePosition++;
-        auto en = Loader::loadEntityAt("Entities/Ant.json", scene, level.spawnPos.x, level.spawnPos.y);
-        en->components.push_back(std::make_shared<PathFollowerComponent>(level));
-        en->components.push_back(std::make_shared<AntAiComponent>(level));
-        scene.addObject(en);
-=======
         if(currentWavePosition < level.waves[currentWave].count) {
             currentWavePosition++;
             auto en = Loader::loadEntityAt("Entities/Ant.json", scene, level.spawnPos.x, level.spawnPos.y);
             en->components.push_back(std::make_shared<PathFollowerComponent>(level));
             en->components.push_back(std::make_shared<HealthBar>());
+            en->components.push_back(std::make_shared<AntAiComponent>(level));
             scene.addObject(en);
         }
->>>>>>> 39fb205334cca551df73eae0e869add4c22a2665
 
         // If this wave is done
         if(currentWavePosition >= level.waves[currentWave].count) {
